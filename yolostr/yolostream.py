0dcb0deb0c102b29050de92d666f452a9887da3e
it-Eng-ma <topbros2024@gmail.com>
2025-05-18 02:02:32 +0100

    Update yolostream.py

--- yolostr/yolostream.py
+++ yolostr/yolostream.py
@@ -75,7 +75,7 @@ if img_file is not None:
     try:
         image = Image.open(img_file).convert("RGB")
 
-        resized_image = image.resize((200, 200))
+        resized_image = image.resize((700 ,448))
         img_array = np.array(resized_image)
 
         results = model.predict(
